53c98d9dcfcbad0b02761a52336e4472b145ab2d
Zet Donaue Samson <zetsamson@gmail.com>
2026-06-15 16:58:36 +0800

    chore: align Ops Ingress path and add ops.dev DNS record to Terraform

--- infrastructure/terraform/environments/dev/02-platform/main.tf
+++ infrastructure/terraform/environments/dev/02-platform/main.tf
@@ -144,6 +144,14 @@ resource "digitalocean_record" "dev" {
   ttl    = 300
 }
 
+resource "digitalocean_record" "ops_dev" {
+  domain = data.terraform_remote_state.infra.outputs.domain_name
+  type   = "A"
+  name   = "ops.dev"
+  value  = module.ingress_controller.load_balancer_ip
+  ttl    = 300
+}
+
 # ------------------------------------------------------------------------------
 # ArgoCD Network Policies
 # ------------------------------------------------------------------------------
